c2b89bfb8ffc42166b7369dc8a320e0d00c8686e
syed <syedmoosa@gmail.com>
2026-05-21 11:40:22 +0500

    Fix product image URL serialization for absolute urls and fallback media serve in production

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,15 +2,41 @@ from rest_framework import serializers
 from .models import Product, Order, Message, StoreSettings, ProductImage, HeroBanner
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = ProductImage
         fields = ['id', 'image']
 
+    def get_image(self, obj):
+        if not obj.image:
+            return None
+        image_str = str(obj.image)
+        if image_str.startswith('http://') or image_str.startswith('https://'):
+            return image_str
+        request = self.context.get('request')
+        if request is not None:
+            return request.build_absolute_uri(obj.image.url)
+        return obj.image.url
+
 class ProductSerializer(serializers.ModelSerializer):
     gallery = ProductImageSerializer(many=True, read_only=True)
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_image(self, obj):
+        if not obj.image:
+            return None
+        image_str = str(obj.image)
+        if image_str.startswith('http://') or image_str.startswith('https://'):
+            return image_str
+        request = self.context.get('request')
+        if request is not None:
+            return request.build_absolute_uri(obj.image.url)
+        return obj.image.url
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/binlibaas/urls.py
+++ backend/binlibaas/urls.py
@@ -4,8 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView
 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
